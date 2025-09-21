f9dc9eb85efaa7c8f4b0c085c8e4aea49d953d70
Antonio Sitoe <antoniositoehl@gmail.com>
2025-09-21 07:23:33 +0200

    setuop inicial do projecto

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+import { getDefaultConfig } from "expo/metro-config";
+import { withNativeWind } from "nativewind/metro";
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+export default withNativeWind(config, {
+  input: "./src/app/globals.css",
+});
